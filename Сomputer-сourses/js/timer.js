955ef87bf22f91a8ed4a6127f892db73756e668e
M1rai1337 <108557370+M1rai1337@users.noreply.github.com>
2022-10-09 23:27:55 +0300

    Computer-courses

---
+++ Сomputer-сourses/js/timer.js
@@ -0,0 +1,25 @@
+const dateFinish = new Date('Oct 25 2022 00:00:00') 
+
+
+function timer(dateFinish){
+    let dateNow = new Date()
+    let dateDifference = dateFinish - dateNow
+
+    let days = Math.floor(dateDifference / 1000 / 60 / 60 / 24)
+    let hours = Math.floor(dateDifference / 1000 / 60 / 60) % 24
+    let minutes = Math.floor(dateDifference / 1000 / 60) % 60
+    let seconds = Math.floor(dateDifference / 1000) % 60
+
+    document.getElementById('days').innerText = days
+    document.getElementById('hours').innerText = hours
+    document.getElementById('minutes').innerText = minutes
+    document.getElementById('seconds').innerText = seconds
+}
+
+timer(dateFinish)
+
+setInterval(() => {
+    timer(dateFinish)
+}, 1000)
+   
+
